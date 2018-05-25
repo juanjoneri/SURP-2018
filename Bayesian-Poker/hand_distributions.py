321f4dd59e540073b5e27be3ef0f9fc3a9477a90
= <=>
2018-05-25 10:46:20 -0700

    implement flush, straight, busted

--- Bayesian-Poker/hand_distributions.py
+++ Bayesian-Poker/hand_distributions.py
@@ -1,5 +1,6 @@
 import itertools
 import random
+import collections
 
 class Card():
     suits = { 'Hearts': '♥', 'Spades': '♣', 'Clubs': '♠', 'Diamonds': '♦'}
@@ -18,6 +19,11 @@ class Card():
     def __eq__(self, other):
         return self.rank == other.rank
 
+    @staticmethod
+    def highest(cards):
+        # returns the highest card by rank in a collection
+        return max(cards)
+
 class Hand():
     size = 5
     def __init__(self, cards):
@@ -25,18 +31,31 @@ class Hand():
             raise IndexError(f'Hands must be composed of {Hand.size} Cards')
         
         self.cards = sorted(cards)
-        self.ranks = {card.rank for card in cards}
-        self.suits = {card.suit for card in cards}
+        self.ranks = {card.rank for card in self.cards}
+        self.suits = {card.suit for card in self.cards}
 
     def __str__(self):
         return str(list(map(str, self.cards)))
 
+    '''
+    Every Hand Type returns the highest card in the hand if the game is present and None otherwise
+    '''
     @property
-    def pairs(self):
-        if len(self.ranks) == (Card.size - 1):
-            pass
-        else:
-            pass
+    def flush(self):
+        if len(self.ranks) == 1:
+            return Card.highest(self.cards)
+        return None
+    
+    @property
+    def straight(self):
+        if list(sorted(self.ranks)) == range(min(self.ranks), max(self.ranks) + 1):
+            return Card.highest(self.cards)
+        return None
+
+    @property
+    def busted(self):
+        if len(self.ranks) == Hand.size and not self.flush and not self.straight:
+            return Card.highest(self.cards)
     
     @property
     def double_pair(self):
@@ -44,13 +63,13 @@ class Hand():
 
 deck = [Card(rank, suit) for rank, suit in itertools.product(Card.ranks, Card.suits)]
 
-
-
 def main():
     hand = Hand(random.sample(deck, 5))
     print(hand)
+    print(Card.highest(hand.cards))
     print(hand.suits)
     print(hand.ranks)
+    print(hand.busted)
 
 if __name__ == '__main__':
     main()
