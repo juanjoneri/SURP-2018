1e26773e169949886f359585b604b75f117a6b79
= <=>
2018-05-25 14:36:09 -0700

    finish implementing possible hands

--- Bayesian-Poker/poker_lib.py
+++ Bayesian-Poker/poker_lib.py
@@ -4,7 +4,8 @@ import collections
 
 class Card():
     suits = { 'Hearts': '♥', 'Spades': '♣', 'Clubs': '♠', 'Diamonds': '♦'}
-    ranks = dict(zip(range(1, 14), '123456789JQKA'))
+    ranks = {i: str(i) for i in range(2, 11)}
+    ranks.update({11: 'J', 12: 'Q', 13: 'K', 14: 'A'})
     
     def __init__(self, rank, suit):
         self.rank = rank
@@ -34,65 +35,67 @@ class Hand():
             raise IndexError(f'Hands must be composed of {Hand.size} Cards')
         
         self.cards = sorted(cards)
-        self.highest = cards[-1]
+        self.counts = collections.Counter(self.cards).items()
+
         self.ranks = {card.rank for card in self.cards}
         self.suits = {card.suit for card in self.cards}
-        # Two can map to same thing when double pair!
-        self.counts = {count: card for card, count in collections.Counter(self.cards).items()}
+        
+    @property
+    def groups(self):
+        self._groups = collections.defaultdict(list)
+        for card, count in self.counts:
+            self._groups[count].append(tuple(filter(lambda x: x == card, self.cards)))
+        return self._groups
 
     def __str__(self):
         return str(list(map(str, self.cards)))
 
     '''
-    Every Hand Type returns the highest card in the hand if the game is present and None otherwise
+    Every Hand Type returns the cards that make up the game if it is present and None otherwise
     '''
     @property
     def flush(self):
         if len(self.ranks) == 1:
-            return self.highest
-        return None
+            return self.cards
+        return []
     
     @property
     def straight(self):
         if list(sorted(self.ranks)) == range(min(self.ranks), max(self.ranks) + 1):
-            return self.highest
-        return None
+            return self.cards
+        return []
 
     @property
     def straight_flush(self):
         if self.flush and self.straight:
-            return self.highest
-        return None
+            return self.cards
+        return []
 
     @property
-    def busted(self):
-        if len(self.ranks) == Hand.size and not self.flush and not self.straight:
-            return self.highest
-    
-    @property
-    def poker(self):
-        try:
-            return self.counts[4]
-        except KeyError: 
-            return None
+    def best_group(self):
+        four_of_a_kind = self.groups[4]
+        three_of_a_kind = self.groups[3]
+        pairs = self.groups[2]
+        busted = self.groups[1]
+        if four_of_a_kind:
+            return four_of_a_kind
+        elif three_of_a_kind and pairs:
+            return three_of_a_kind.update(paris)
+        elif three_of_a_kind:
+            return three_of_a_kind
+        elif pairs:
+            return pairs
+        else:
+            return busted
 
-    @property
-    def pair(self):
-        try:
-            return self.counts[2]
-        except KeyError: 
-            return None
-    
-    @property
-    def double_pair(self):
-        return len(self.ranks) == (Card.size - 2)
 
 deck = [Card(rank, suit) for rank, suit in itertools.product(Card.ranks, Card.suits)]
 
 def main():
     hand = Hand(random.sample(deck, 5))
+    print(len(deck))
     print(hand)
-    print(hand.pair)
+    print(hand.best_group)
     # print(Card.highest(hand.cards))
     # print(hand.suits)
     # print(hand.ranks)
