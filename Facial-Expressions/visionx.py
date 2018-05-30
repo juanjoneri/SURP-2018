73ff86d900b9ddd2bd50db05f259d558907da3f8
= <=>
2018-05-30 01:56:45 +0000

    make a library for finding labels in images

--- Facial-Expressions/visionx.py
+++ Facial-Expressions/visionx.py
@@ -2,7 +2,7 @@ import io
 from google.cloud import vision
 from google.cloud.vision import types
 
-path_to_image = './guido.jpg'
+path_to_image = './joy.jpg'
 
 def detect_face(face_file, max_results=4):
     client = vision.ImageAnnotatorClient()
@@ -10,10 +10,27 @@ def detect_face(face_file, max_results=4):
     content = face_file.read()
     image = types.Image(content=content)
 
-    return client.face_detection(image=image).face_annotations
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    return labels
 
+def detect_labels_uri(uri):
+    """Detects labels in the file located in Google Cloud Storage or on the
+    Web."""
+    client = vision.ImageAnnotatorClient()
+    image = types.Image()
+    image.source.image_uri = uri
 
-with io.open(path_to_image, 'rb') as image_file:
-    response = detect_face(image_file)
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
 
-print(response)
+    return labels:
+
+#with io.open(path_to_image, 'rb') as image_file:
+#   labels = detect_face(image_file)
+
+#for label in labels:
+#    print(labels)
+
+if __name__ == '__main__':
+    detect_labels_uri('https://media.mnn.com/assets/images/2016/10/joy.jpg.653x0_q80_crop-smart.jpg')
