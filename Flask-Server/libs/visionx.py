07468bbdc0d2d1f0737277d9d901a96c08ecaf86
= <=>
2018-05-29 19:33:30 -0700

    accept request for user image url

---
+++ Flask-Server/libs/visionx.py
@@ -0,0 +1,38 @@
+import io
+from google.cloud import vision
+from google.cloud.vision import types
+
+path_to_image = './joy.jpg'
+
+def detect_face(face_file, max_results=4):
+    client = vision.ImageAnnotatorClient()
+
+    content = face_file.read()
+    image = types.Image(content=content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    return labels
+
+def detect_labels_uri(uri):
+    """Detects labels in the file located in Google Cloud Storage or on the
+    Web."""
+    client = vision.ImageAnnotatorClient()
+    image = types.Image()
+    image.source.image_uri = uri
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    return labels
+
+#with io.open(path_to_image, 'rb') as image_file:
+#   labels = detect_face(image_file)
+
+#for label in labels:
+#    print(labels)
+
+if __name__ == '__main__':
+    labels = detect_labels_uri('https://media.mnn.com/assets/images/2016/10/joy.jpg.653x0_q80_crop-smart.jpg')
+    for label in labels:
+        print(label.description)
--- Flask-Server/main.py
+++ Flask-Server/main.py
@@ -1,15 +1,22 @@
 import logging
-from ../Facial-Expression/visionx.py import detect_labels_uri
-
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    labels = detect_labels_uri('https://www.pokersites.ca/images/top-players/daniel-negreanu-sm.jpg')
-    return f'{labels[0].description}'
+    return app.send_static_file('index.html')
 
+@app.route('/facial-expression')
+def facial_expression():
+    #labels = visionx.detect_labels_uri('https://www.pokersites.ca/images/top-players/daniel-negreanu-sm.jpg')
+    image_url = request.args.get('image_url')
+    return \
+    '''
+    <h1>The image at {}</h1>
+    <h2>has the following tags:</h2>
+    <p>{}</p>
+    '''.format(image_url, 'some label')
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=8080, debug=True)
