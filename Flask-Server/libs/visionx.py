67cebbf94e0f38f20be3c4f96c3b9ee5ebcdd426
= <=>
2018-05-30 10:35:54 -0700

    display usefule labels for poker bot

--- Flask-Server/libs/visionx.py
+++ Flask-Server/libs/visionx.py
@@ -14,6 +14,20 @@ def detect_face(face_file, max_results=4):
     labels = response.label_annotations
     return labels
 
+def detect_joy(uri):
+    client = vision.ImageAnnotatorClient()
+    image = types.Image()
+    image.source.image_uri = uri
+    response = client.face_detection(image=image)
+    confidence = {
+        'joy': response.face_annotations[0].joy_likelihood,
+        'anger': response.face_annotations[0].anger_likelihood,
+        'surprise': response.face_annotations[0].surprise_likelihood,
+        'sorrow': response.face_annotations[0].sorrow_likelihood
+    }
+    return confidence
+    
+
 def detect_labels_uri(uri):
     """Detects labels in the file located in Google Cloud Storage or on the
     Web."""
--- Flask-Server/main.py
+++ Flask-Server/main.py
@@ -1,6 +1,6 @@
 import logging
 from flask import Flask, request
-from libs.visionx import detect_labels_uri
+from libs.visionx import detect_labels_uri, detect_joy
 
 app = Flask(__name__)
 
@@ -11,14 +11,14 @@ def homepage():
 @app.route('/facial-expression')
 def facial_expression():
     image_url = request.args.get('image_url')
-    labels = detect_labels_uri(image_url)
+    joy_list = detect_joy(image_url)
 
     return \
     '''
     <h1>The image at {}</h1>
-    <h2>has the following tags:</h2>
+    <h2>displays the following visual queues from 1 to 5:</h2>
     <p>{}</p>
-    '''.format(image_url, list(map(lambda label: (label.description, label.score), labels)))
+    '''.format(image_url, joy_list)
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=8080, debug=True)
