7d73ebc8e19d75809f14fc3c30dcea4e95480254
Juan Neri <juanjo.neri@gmail.com>
2018-05-30 22:09:47 -0700

    typo that made sms not work

--- Flask-Server/main.py
+++ Flask-Server/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     visitor_ip = request.remote_addr
-    send_sms_to('../nexmokey.json', 'Someone from {} is in poker-bot-v1s home page'.format(visitor_id), 12132745685)
+    send_sms_to('../nexmokey.json', 'Someone from {} is in poker-bot-v1s home page'.format(visitor_ip), 12132745685)
     return app.send_static_file('index.html')
 
 @app.route('/facial-expression')
