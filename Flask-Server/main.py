67ad7df0c63f270803e40a8f6f837d93b8e59373
Juan Neri <juanjo.neri@gmail.com>
2018-06-08 09:36:25 -0700

    finish interface 1.0

--- Flask-Server/main.py
+++ Flask-Server/main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def homepage():
     return app.send_static_file('index.html')
 
+@app.route('/instructions')
+def instruction():
+    return app.send_static_file('instructions.html')
+
 @app.route('/facial-expression')
 def facial_expression():
     image_url = request.args.get('image_url')
