803cf1e00166ccff3b69f2cd44c415335a95ef42
Juan Neri <juanjo.neri@gmail.com>
2018-06-08 08:56:21 -0700

    refactor css and js

---
+++ Upload-Files/script.js
@@ -0,0 +1,17 @@
+
+
+
+function browseFile(input, output) {
+  console.log(output)
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $(`#${output}`)
+                .attr('src', e.target.result)
+                .width(350)
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
