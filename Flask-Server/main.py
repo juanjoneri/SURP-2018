a74d1c0faf120a07541e837782354f02967909a2
Juan Neri <juanjo.neri@gmail.com>
2018-06-08 11:21:50 -0700

    save images to server

--- Flask-Server/main.py
+++ Flask-Server/main.py
@@ -1,12 +1,35 @@
 import logging
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for
 from libs.visionx import detect_labels_uri, detect_joy
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'flac', 'wav'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+        filename = file.name
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        extension = file.filename.rsplit('.', 1)[1].lower()
+        if extension in ALLOWED_EXTENSIONS:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'cards.{}'.format(extension)))
+    
     return app.send_static_file('index.html')
+
 
 @app.route('/instructions')
 def instruction():
