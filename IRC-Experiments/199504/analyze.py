7186d1190f5b4e8ac6922ed1bad7b84705acebca
Juan Neri <Juanjo.neri@gmail.com>
2018-06-21 14:57:08 -0700

    join hands and rosters

--- IRC-Experiments/199504/analyze.py
+++ IRC-Experiments/199504/analyze.py
@@ -18,23 +18,13 @@ rosters_headers = ('timestamp', 'nb_players', 'players')
 actions_names = ('player', 'timestamp', 'nb_players', 'first_player', 'preflop', 'flop', 'turn', 'river', 
                  'bankroll', 'total_action', 'pot_won', 'cards')
 
-"""
-column 1        player nickname
-column 2        timestamp of this hand (see HDB)
-column 3        number of player dealt cards
-column 4        position of player (starting at 1, in order of cards received)
-column 5        betting action preflop (see below)
-column 6        betting action on flop (see below)
-column 7        betting action on turn (see below)
-column 8        betting action on river (see below)
-column 9        player's bankroll at start of hand
-column 10       total action of player during hand
-column 11       amount of pot won by player
-column 12+      pocket cards of player (if revealed at showdown)
-"""
-
-hands = pd.read_csv('Results/hdb.csv', names=hands_headers)
-rosters = pd.read_csv('Results/hroster.csv', skipinitialspace=True, quotechar='"', names=roster_headers)
+hands = pd.read_csv('Results/hdb.csv', names=hands_headers).set_index('timestamp')
+rosters = pd.read_csv('Results/hroster.csv', skipinitialspace=True, quotechar='"', names=rosters_headers).set_index('timestamp')
 actions = pd.read_csv('Results/pdb.csv', skipinitialspace=True, quotechar='"', names=actions_names)
 
-print(actions)
+## join hands and rosters, dropping duplicate column: nb_players
+rosters = rosters.drop(['nb_players'], axis=1)
+rosters = rosters.join(hands)
+
+
+print(rosters)
