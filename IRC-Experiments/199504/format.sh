093c18dbda61496638bb88a37d0307ed5fbdd53a
Juan Neri <Juanjo.neri@gmail.com>
2018-06-21 10:01:40 -0700

    final cleaning of format

--- IRC-Experiments/199504/format.sh
+++ IRC-Experiments/199504/format.sh
@@ -3,18 +3,16 @@
 # 2. hroster
 # 3. pdb/*
 
-# Put a backslash before $.*/[\]^ (but not inside bracket expressions).
-
-
+# output directory
 mkdir Results
 
 # HANDS
-# convert hands csv             
+# convert hands csv
 sed -E 's/([0-9]) /\1, /g' hdb > Results/hdb.csv
 # split the flop, turn, etc
 sed -i -E 's/\//, /g' Results/hdb.csv
 # rm temporary file
-rm Results/hdb.csv-E 
+rm Results/hdb.csv-E
 
 # ROSTER
 # convert roster csv
@@ -22,14 +20,14 @@ sed -E "s/([0-9A-Za-z]) /\1, /g" hroster > Results/hroster.csv
 # group users into one column
 sed -i -E 's/ \([0-9]\{1,2\}\), \(.*\)$/\1, [\2]/g' Results/hroster.csv
 # rm temporary file
-rm Results/hroster.csv-E 
+rm Results/hroster.csv-E
 
 # ACTIONS
 # concat all pdb files
-cat pdb/pdb.* >> Results/pdb.csv 
+cat pdb/pdb.* >> Results/pdb.csv
 # convert to csv
 sed -i -E 's/\([-0-9A-Za-z]\) /\1, /g' Results/pdb.csv
-# remove temp
+# rm temporary file
 rm Results/pdb.csv-E
 
 echo Finished
