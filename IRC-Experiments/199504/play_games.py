fc9f6e3480aaab43b6dff273e66d502792778f6f
Juan Neri <Juanjo.neri@gmail.com>
2018-06-22 10:37:10 -0700

    create texas player class, working on texas game class

--- IRC-Experiments/199504/play_games.py
+++ IRC-Experiments/199504/play_games.py
@@ -7,24 +7,46 @@ headers = [
         'total_action', 'pot_won', 'cards',
         ]
 
+from deuces import Deck, Card, Evaluator
 
 class TexasPlayer():
-    def __init__(self, df_row):
-        pass
+    ACTIONS = dict(zip('Bcfrkb', ('blind', 'call', 'fold', 'raise', 'check', 'bet')))
 
-class TexasGame():
-    def __init__(self, player1, player2, df_row):
-        self.board = df_row['board'].item().strip().split(' ')
-        self.players = df_row['players'].item().split(',')
+    def __init__(self, df_row):
+        self.name = df_row['player'].item()
+        self.pot_won = int(df_row['pot_won'].item())
+        self.cards = [Card.new(value) for value in df_row['cards'].item().split(', ')]
+
+        self.actions = [
+            df_row['preflop'].item(),
+            df_row['flop'].item(),
+            df_row['turn'].item(),
+            df_row['river'].item(),
+            ]
 
     def __str__(self):
-        return str(self.board)
+        return self.name
+
+    @property
+    def won(self):
+        return self.pot_won > 0
+
+class TexasGame():
+    def __init__(self, p1, p2, df_row):
+        self.board = [Card.new(value) for value in df_row['board'].item().strip().split(' ')]
+        self.players = (p1, p2) if int(df_row['first_player'].item()) == 1 else (p2, p1)
+
+    def __str__(self):
+        return f'{self.players[0]} vs {self.players[1]}'
 
 if __name__ == '__main__':
     df = pd.read_pickle('Results/dataframe.pkl')
     rows, cols = df.shape
  
+    print(df.head(10))
     for i in range(0, 10, 2):
-        player1, player2 = df.loc[[i]], df.loc[[i+1]]
-        game = TexasGame(player1, player2, player1)
+        row1, row2 = df.loc[[i]], df.loc[[i+1]]
+        p1 = TexasPlayer(row1)
+        p2 = TexasPlayer(row2)
+        game = TexasGame(p1, p2, row1)
         print(game)
